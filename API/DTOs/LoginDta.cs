0b9e13b76007a0f8d19d5f6d433aa5346d8351ba
Funmbi <bammieyour@gmail.com>
2020-10-16 00:03:28 +0100

    End Of Section Four

---
+++ API/DTOs/LoginDta.cs
@@ -0,0 +1,8 @@
+namespace API.DTOs
+{
+    public class LoginDta
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,9 @@
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        public string username {get; set;}
+
+        public string password { get; set; }
+    }
+}
